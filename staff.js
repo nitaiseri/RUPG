264232e063dab2be6a5cc825e697e56387e8977a
Nitai Seri <Nitai.ns@gmail.com>
2022-09-11 09:03:45 +0300

    end

---
+++ staff.js
@@ -0,0 +1,11 @@
+"use strict";
+// let json = await $.getJSON("mock_users.json");
+// let index = Math.floor(Math.random()*json.length)
+// let newDataUser = json[index]["results"][0]
+// return [{ firstName: "1", lastName: "a" },
+// { firstName: "2", lastName: "b" },
+// { firstName: "3", lastName: "c" },
+// { firstName: "4", lastName: "d" },
+// { firstName: "5", lastName: "e" },
+// { firstName: "6", lastName: "f" },
+// { firstName: "7", lastName: "g" }]
